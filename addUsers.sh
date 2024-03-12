20bfced622f768c9ae82d1e6d409b2e4402f7b82
Devist <34066247+CistusF@users.noreply.github.com>
2024-03-12 15:09:12 +0900

    Add files via upload

---
+++ addUsers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+names=("boser" "edgar" "yarano" "vincent" "trevo" "haper" "janela" "ellie" "harper" "wilson" "edwin" "dewey" "devan" "caley" "kori" "qadani" "willy" "qimat" "dexter" "linda" "dana" "jasmin" "genevi" "noner" "adel" "robert" "xandra" "dante" "julia" "ximena" "tania" "miller" "ringo" "lavina" "yummer" "visiter" "dale" "damber" "snower" "david" "shimber" "ouker" "surer" "nower" "davido" "owner" "kimber" "jini" "choser" "kelly" "yester" "siniker", "sabrina")
+myname="sabrina"
+PASS="wjsansrk"
+
+for i in "${names[@]}"; do
+  if [$i == sabrina]; then
+    continue
+  fi 
+  adduser $i
+  echo "$PASS" | passwd "$i" --stdin
+done
