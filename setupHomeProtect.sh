4fc3e36fdbb2fa61547fc9c32ad3fa898e564a01
Vendetta <ananddechannel@gmail.com>
2024-03-19 12:03:03 +0900

    change vi to move for automatically updating

--- setupHomeProtect.sh
+++ setupHomeProtect.sh
@@ -6,7 +6,8 @@ chmod 711 /home
 #vsftpd home protect
 yum -y install vsftpd
 
-read -p "Find #chroot_local_user=YES\nDelete #"
 
-sudo vi /etc/vsftpd/vsftpd.conf
+mv /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf
+# read -p "Find #chroot_local_user=YES\nDelete #"
+# sudo vi /etc/vsftpd/vsftpd.conf
 systemctl restart vsftpd
